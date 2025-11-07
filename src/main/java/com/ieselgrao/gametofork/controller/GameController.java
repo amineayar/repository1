f5398042d84b81fbd0048316d8e99c55b5c11fd2
amine <ayaramine34@gmail.com>
2025-11-07 18:45:53 +0100

    Feat: Adjust game circle parameters for improved gameplay

--- src/main/java/com/ieselgrao/gametofork/controller/GameController.java
+++ src/main/java/com/ieselgrao/gametofork/controller/GameController.java
@@ -27,10 +27,10 @@ public class GameController {
     private Random random = new Random();
 
     // Parámetros de los círculos
-    private final double MIN_RADIUS = 10;
-    private final double MAX_RADIUS = 30;
-    private final double FALL_SPEED = 1;
-    private final double LOST_LINE_Y = 550; // Línea cerca del pie de la ventana (600px)
+    private final double MIN_RADIUS = 4;
+    private final double MAX_RADIUS = 9;
+    private final double FALL_SPEED = 3;
+    private final double LOST_LINE_Y = 999; // Línea cerca del pie de la ventana (600px)
 
     @FXML
     public void initialize() {
